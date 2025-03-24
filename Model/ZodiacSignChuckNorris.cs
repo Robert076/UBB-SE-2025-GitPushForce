45c5e31982c3b020e2de72c59acdb9d22911e7c4
Boar Victor <rotcivraob@yahoo.com>
2025-03-24 19:13:30 +0200

    UserZodiacSign elements, gets, sets and sign compute

--- Model/ZodiacSignChuckNorris.cs
+++ Model/ZodiacSignChuckNorris.cs
@@ -13,11 +13,11 @@ namespace src.Model
         private string _signUser;
         private decimal _amountCredit;
 
-        public UserZodiacSign(string cnpUser, DateTime birthDate_user, string signUser, decimal amountCredit)
+        public UserZodiacSign(string cnpUser, DateTime birthDate_user, decimal amountCredit)
         {
             _cnpUser = cnpUser;
             _birthDate_user = birthDate_user;
-            _signUser = signUser;
+            _signUser = CalculateZodiacSign(birthDate_user);
             _amountCredit = amountCredit;
         }
 
@@ -25,7 +25,7 @@ namespace src.Model
         {
             _cnpUser = string.Empty;
             _birthDate_user = DateTime.Now;
-            _signUser = string.Empty;
+            _signUser = CalculateZodiacSign(DateTime.Now);
             _amountCredit = 0;
         }
 
@@ -38,19 +38,70 @@ namespace src.Model
         public DateTime BirthDate_user
         {
             get { return _birthDate_user; }
-            set { _birthDate_user = value; }
+            set { 
+                  _birthDate_user = value; 
+                  _signUser = CalculateZodiacSign(value);
+            }
         }
 
         public string SignUser
         {
             get { return _signUser; }
-            set { _signUser = value; }
+            //set { _signUser = value; }
         }
 
         public decimal AmountCredit
         {
             get { return _amountCredit; }
             set { _amountCredit = value; }
+        }
+
+        private string CalculateZodiacSign(DateTime birthDate)
+        {
+
+            int day = birthDate.Day;
+            int month = birthDate.Month;
+
+            if ((month == 3 && day >= 21) || (month == 4 && day <= 19))
+                return "Berbec";
+
+            if ((month == 4 && day >= 20) || (month == 5 && day <= 20))
+                return "Taur";
+
+            if ((month == 5 && day >= 21) || (month == 6 && day <= 20))
+                return "Gemeni";
+
+            if ((month == 6 && day >= 21) || (month == 7 && day <= 22))
+                return "Rac";
+
+            if ((month == 7 && day >= 23) || (month == 8 && day <= 22))
+                return "Leu";
+
+            if ((month == 8 && day >= 23) || (month == 9 && day <= 22))
+                return "Fecioara";
+
+            if ((month == 9 && day >= 23) || (month == 10 && day <= 22))
+                return "Balanta";
+
+            if ((month == 10 && day >= 23) || (month == 11 && day <= 21))
+                return "Scorpion";
+
+            if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+                return "Sagetator";
+
+            if ((month == 12 && day >= 22) || (month == 1 && day <= 19))
+                return "Capricorn";
+
+            if ((month == 1 && day >= 20) || (month == 2 && day <= 18))
+                return "Varsator";
+
+            if ((month == 2 && day >= 19) || (month == 3 && day <= 20))
+                return "Pesti";
+
+
+            return "Capricorn";
+
+
         }
     }
 }
