c4745793b312d951bdafad5724a706123c123576
Boar Victor <rotcivraob@yahoo.com>
2025-03-24 18:40:12 +0200

    UserZodiacSign elements

---
+++ Model/ZodiacSignChuckNorris.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Model
+{
+    public class UserZodiacSigngit
+    {
+        public string cnpUser { get; set; }
+        public DateTime birthDate_user { get; set; }
+        public string signUser { get; set; }
+        public decimal amountCredit { get; set; }
+
+
+
+    }
+}
