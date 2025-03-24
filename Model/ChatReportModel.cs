63e67420c53f16affb769c055b2e65f6b15a7ec6
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-03-24 17:53:30 +0200

    fields and methods for chat report

--- Model/ChatReportModel.cs
+++ Model/ChatReportModel.cs
@@ -8,5 +8,40 @@ namespace src.Model
 {
     public class ChatReportModel
     {
+        private int _id;
+        private string _reportedUserCNP;
+        private string _reportedMessage;
+
+        public ChatReportModel(int id, string reportedUserCNP, string reportedMessage)
+        {
+            _id = id;
+            _reportedUserCNP = reportedUserCNP;
+            _reportedMessage = reportedMessage;
+        }
+
+        public ChatReportModel()
+        {
+            _id = 0;
+            _reportedUserCNP = "";
+            _reportedMessage = "";
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string ReportedUserCNP
+        {
+            get { return _reportedUserCNP; }
+            set { _reportedUserCNP = value; }
+        }
+
+        public string ReportedMessage
+        {
+            get { return _reportedMessage; }
+            set { _reportedMessage = value; }
+        }
     }
 }
