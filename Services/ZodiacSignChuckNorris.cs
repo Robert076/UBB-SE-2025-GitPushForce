e3ad2db4d2ad7a6cb8e637e71ec4407b79e7d167
Boar Victor <rotcivraob@yahoo.com>
2025-03-25 15:03:11 +0200

    feat\Boar Victor\ Zodiac sign services class creation

---
+++ Services/ZodiacSignChuckNorris.cs
@@ -0,0 +1,20 @@
+﻿using src.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Services
+{
+    class ZodiacSignChuckNorrisService
+    {
+        public UserZodiacSign ZodiacSignModel;
+
+        public ZodiacSignChuckNorrisService()
+        {
+            this.ZodiacSignModel= new UserZodiacSign(); 
+        }
+
+    }
+}
