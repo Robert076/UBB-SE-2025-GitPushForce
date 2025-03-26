1f861baff95562fc2ed91505a2ed0ad816bebfed
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-03-26 21:27:27 +0200

    execute scalar method

--- Data/DatabaseConnection.cs
+++ Data/DatabaseConnection.cs
@@ -38,7 +38,38 @@ namespace src.Data
                 sqlConnection.Close();
             }
         }
+        
+        public T? ExecuteScalar<T> (string storedProcedure, SqlParameter[]? sqlParameters = null)
+        {
+            try
+            {
+                OpenConnection();
+                using (SqlCommand command = new SqlCommand(storedProcedure, sqlConnection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
 
+                    if (sqlParameters != null)
+                    {
+                        command.Parameters.AddRange(sqlParameters);
+                    }
 
+                    var result = command.ExecuteScalar();
+                    if(result == DBNull.Value || result == null)
+                    {
+                        return default;
+                    }
+
+                    return (T)Convert.ChangeType(result, typeof(T));
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Error - ExecutingScalar: {exception.Message}");
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
     }
 }
