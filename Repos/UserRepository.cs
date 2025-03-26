37abc3e4aefbc5ee35d54252775e6f40cb4b6581
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-03-26 22:17:34 +0200

    repo setup

---
+++ Repos/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using src.Data;
+
+namespace src.Repos
+{
+    public class UserRepository
+    {
+        private readonly DatabaseConnection dbConn;
+    }
+}
