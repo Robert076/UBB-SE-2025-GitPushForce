f8f0f54db9f6c1ff3aa793324293f7d7cd0532bf
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-03-26 21:16:22 +0200

    constructor for databaseconnection

---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+
+namespace src.Data
+{
+    public class DatabaseConnection
+    {
+        private SqlConnection sqlConnection;
+        private readonly string connectionString;
+
+        public DatabaseConnection()
+        {
+            connectionString = "Server=localhost\\SQLEXPRESS;Database=GitPushForce;Trusted_Connection=True;TrustServerCertificate=True;";
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+            } 
+            catch(Exception exception)
+            {
+                throw new Exception($"Error initializing SQL Connection {exception.Message}");
+            }
+
+        }
+    }
+}
