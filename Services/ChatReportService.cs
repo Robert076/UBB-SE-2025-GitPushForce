540cd210b13b88ca4c95c23f77d3cbca67e521bb
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-03-28 13:05:58 +0200

    moving on to connection to api

--- Services/ChatReportService.cs
+++ Services/ChatReportService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using src.Repos;
+using src.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace src.Services
 {
     public class ChatReportService
     {
+        ChatReportRepository _chatReportRepository;
+
+        public ChatReportService(ChatReportRepository chatReportRepository)
+        {
+            _chatReportRepository = chatReportRepository;
+        }
+        static public void SolveChatReport(ChatReport chatReportToBeSolved)
+        {
+            string reportedMessage = chatReportToBeSolved.ReportedMessage;
+
+            bool isReportedMessageOffensive = 
+        }
+        static public bool IsMessageOffensive(string messageToBeChecked)
+        {
+            return true;
+        }
     }
 }
