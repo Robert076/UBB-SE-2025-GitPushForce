347d338498d74aba52f499c84dfa348c9f26ad96
laithha <147397351+laithha@users.noreply.github.com>
2025-03-28 21:17:37 +0200

    did the messages and the tips pr

---
+++ Model/Messages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Model
+{
+    public class Messages
+    {
+        private int _id;
+        private string _type; 
+        private string _message;
+
+        public Messages(int id, string type, string message)
+        {
+            _id = id;
+            _type = type;
+            _message = message;
+        }
+
+        public Messages()
+        {
+            _id = 0;
+            _type = string.Empty;
+            _message = string.Empty;
+        }
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+    }
+}
---
+++ Model/Tips.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Model
+{
+    class Tips
+    {
+        private int _id;
+        private String _creditScoreBracket;
+        private String _tipText;
+
+        public Tips(int id, String creditScoreBracket, String tipText)
+        {
+            _id = id;
+            _creditScoreBracket = creditScoreBracket;
+            _tipText = tipText;
+        }
+
+        public Tips()
+        {
+            _id = 0;
+            _creditScoreBracket = string.Empty;
+            _tipText = string.Empty;
+        }
+
+        public int id
+        {
+            get { return _id;  }
+            set { _id = value; }
+        }
+
+        public String creditScoreBracket
+        {
+            get { return _creditScoreBracket; }
+            set { _creditScoreBracket = value; }
+        }
+
+        public String tipText
+        {
+            get { return _tipText; }
+            set { _tipText = value; }
+        }
+    }
+}
