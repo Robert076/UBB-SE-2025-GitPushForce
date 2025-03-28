90caee97b1cf8dc1d1df30fb25150bdcb9ca66ad
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-28 16:34:39 +0200

    Creating the repo for history and create the function for getting data from database

---
+++ Repos/HistoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using src.Data;
+using src.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Repos
+{
+    public class HistoryRepository
+    {
+
+        private readonly DatabaseConnection dbConn;
+
+        public HistoryRepository(DatabaseConnection dbConn)
+        {
+            this.dbConn = dbConn;
+        }
+
+        public List<HistoryCreditScore> GetHistoryForUser(string userCNP)
+        {
+            if (string.IsNullOrWhiteSpace(userCNP))
+            {
+                throw new ArgumentException("Invalid CNP");
+            }
+
+            SqlParameter[] parameters = new SqlParameter[] {
+                new SqlParameter("@UserCNP", userCNP)
+            };
+
+            
+        }
+    }
+}
