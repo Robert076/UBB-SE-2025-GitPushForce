3af1c307aba172d5367316e5c74a627ec8437189
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-28 17:53:25 +0200

    history service with get history list method

---
+++ Services/HistoryService.cs
@@ -0,0 +1,32 @@
+﻿using src.Model;
+using src.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Services
+{
+    public class HistoryService
+    {
+        private readonly HistoryRepository _historyRepository;
+
+        public HistoryService(HistoryRepository historyRepository)
+        {
+            _historyRepository = historyRepository ?? throw new ArgumentNullException(nameof(historyRepository));
+        }
+
+        public List<HistoryCreditScore> GetHistoryByUserId(string userCNP)
+        {
+            if (string.IsNullOrWhiteSpace(userCNP))
+            {
+                throw new ArgumentException("User ID cannot be null");
+            }
+            return _historyRepository.GetHistoryForUser(userCNP);
+        }
+
+
+
+    }
+}
