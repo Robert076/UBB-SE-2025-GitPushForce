7f408fa739c0f9cc849d7e00be6aba2450c88429
Boar Victor <rotcivraob@yahoo.com>
2025-03-29 15:06:41 +0200

    feat/victor/Zodiacrepository

---
+++ Repos/ZodiacRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using src.Data;
+using src.Model;
+
+namespace src.Repos
+{
+    class ZodiacRepository
+    {
+        private readonly DatabaseConnection dbConn;
+
+        public ZodiacRepository(DatabaseConnection dbConn)
+        {
+            this.dbConn = dbConn;
+        }
+
+
+
+
+    }
+}
