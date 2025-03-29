3794ae2c39f720afb0acafab26fbb44117035838
paulb <paulberca@gmail.com>
2025-03-29 23:06:51 +0200

    loading the loan request components on the UI

--- View/Pages/LoanRequestView.xaml.cs
+++ View/Pages/LoanRequestView.xaml.cs
@@ -12,6 +12,11 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using src.Data;
+using src.Repos;
+using src.Services;
+using src.Model;
+using src.View.Components;
 
 namespace src.Views
 {
@@ -20,6 +25,36 @@ namespace src.Views
         public LoanRequestView()
         {
             this.InitializeComponent();
+            LoadLoanRequests();
+        }
+
+        private void LoadLoanRequests()
+        {
+            LoanRequestContainer.Items.Clear(); // Clear previous items before reloading
+
+            DatabaseConnection dbConn = new DatabaseConnection();
+            LoanRequestRepository repo = new LoanRequestRepository(dbConn);
+            LoanRequestService service = new LoanRequestService(repo);
+
+            try
+            {
+                List<LoanRequest> loanRequests = service.GetLoanRequests();
+
+                foreach (var request in loanRequests)
+                {
+                    LoanRequestComponent requestComponent = new LoanRequestComponent();
+                    requestComponent.SetRequestData(request.RequestID, request.UserCNP, request.Amount);
+
+                    // Subscribe to the event to refresh when a request is solved
+                    //requestComponent.LoanSolved += OnLoanSolved;
+
+                    LoanRequestContainer.Items.Add(requestComponent);
+                }
+            }
+            catch (Exception)
+            {
+                LoanRequestContainer.Items.Add("There are no loan requests that need solving.");
+            }
         }
     }
 }
