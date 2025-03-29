b4cb1ee614be922e26a96a06a6a0061410f80745
Boar Victor <rotcivraob@yahoo.com>
2025-03-29 15:34:08 +0200

    feat/victor/ZodiacRepository public class

--- Repos/ZodiacRepository.cs
+++ Repos/ZodiacRepository.cs
@@ -10,7 +10,7 @@ using Microsoft.Data.SqlClient;
 
 namespace src.Repos
 {
-    class ZodiacRepository
+    public class ZodiacRepository
     {
         private readonly DatabaseConnection dbConn;
 
