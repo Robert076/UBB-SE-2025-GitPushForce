5ee113498415c5ee09acb05c772ea6b768e19cbf
Boar Victor <rotcivraob@yahoo.com>
2025-03-29 15:09:21 +0200

    feat/victor/ZodiacRepository

--- Repos/ZodiacRepository.cs
+++ Repos/ZodiacRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using src.Data;
 using src.Model;
+using Microsoft.Data.SqlClient;
 
 namespace src.Repos
 {
@@ -18,6 +20,32 @@ namespace src.Repos
         }
 
 
+        public int CreateZodiacModel(ZodiacModel zodiacModel)
+        {
+            if (zodiacModel == null)
+            {
+                throw new ArgumentNullException(nameof(zodiacModel), "ZodiacModel cannot be null.");
+            }
+
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@CNP", zodiacModel.CNP),
+                new SqlParameter("@Birthday", zodiacModel.Birthday.ToString("yyyy-MM-dd")),
+                new SqlParameter("@CreditScore", zodiacModel.CreditScore),
+                new SqlParameter("@ZodiacSign", zodiacModel.ZodiacSign),
+                new SqlParameter("@ZodiacAttribute", zodiacModel.ZodiacAttribute)
+            };
+
+            try
+            {
+                int? result = dbConn.ExecuteScalar<int>("CreateZodiacModel", parameters, CommandType.StoredProcedure);
+                return result ?? 0;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception($"Database error: {ex.Message}");
+            }
+        }
 
 
     }
