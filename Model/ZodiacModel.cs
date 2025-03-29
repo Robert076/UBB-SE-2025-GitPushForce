af2d47f7293c9fe6722b346d1b00075280495016
Boar Victor <rotcivraob@yahoo.com>
2025-03-29 14:49:52 +0200

    feat/victor/ZodiacModel bilder

--- Model/ZodiacModel.cs
+++ Model/ZodiacModel.cs
@@ -14,5 +14,29 @@ namespace src.Model
         public int _CreditScore { get; set; }
         public string _ZodiacSign { get; set; }
         public string _ZodiacAttribute { get; set; }
+
+
+        public ZodiacModel(int id, string cnp, DateOnly birthday, int creditScore, string zodiacSign, string zodiacAttribute)
+        {
+            _Id = id;
+            _CNP = cnp;
+            _Birthday = birthday;
+            _CreditScore = creditScore;
+            _ZodiacSign = zodiacSign;
+            _ZodiacAttribute = zodiacAttribute;
+        }
+
+        public ZodiacModel()
+        {
+            _Id = 0;
+            _CNP = string.Empty;
+            _Birthday = new DateOnly();
+            _CreditScore = 0;
+            _ZodiacSign = string.Empty;
+            _ZodiacAttribute = string.Empty;
+        }
+
     }
+
+    
 }
