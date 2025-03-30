64551c631be0dbc0e508c7890b9d574c238687f5
paulb <paulberca@gmail.com>
2025-03-31 02:22:05 +0300

    changed this so that it refreshes the loans based on the timer

--- View/Pages/LoansView.xaml.cs
+++ View/Pages/LoansView.xaml.cs
@@ -1,53 +1,55 @@
- using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using src.Data;
 using src.Repos;
 using src.Services;
 using src.Model;
 using src.View.Components;
+using System.Collections.Generic;
+using System;
 
 namespace src.Views
 {
     public sealed partial class LoansView : Page
     {
+        private readonly LoanServices _service;
+        private readonly LoanCheckerService _loanCheckerService;
+
         public LoansView()
         {
             this.InitializeComponent();
+
+            DatabaseConnection dbConn = new DatabaseConnection();
+            LoanRepository loanRepository = new LoanRepository(dbConn);
+            _service = new LoanServices(loanRepository);
+
+            _loanCheckerService = new LoanCheckerService(_service);
+            _loanCheckerService.LoansUpdated += OnLoansUpdated;
+            _loanCheckerService.Start(); // Start periodic checking
+
             LoadLoans();
+        }
+
+        private void OnLoansUpdated(object sender, EventArgs e)
+        {
+            LoadLoans(); // Refresh the list instantly when a request is solved
         }
 
         private void LoadLoans()
         {
             LoansContainer.Items.Clear(); // Clear previous items before reloading
 
-            DatabaseConnection dbConn = new DatabaseConnection();
-            LoanRepository loanRepository = new LoanRepository(dbConn);
-            LoanServices service = new LoanServices(loanRepository);
-
             try
             {
-                List<Loan> loans = service.GetLoans();
+                List<Loan> loans = _service.GetLoans();
                 foreach (Loan loan in loans)
                 {
                     LoanComponent loanComponent = new LoanComponent();
                     loanComponent.SetLoanData(loan.LoanID, loan.UserCNP, loan.LoanAmount, loan.ApplicationDate,
                                               loan.RepaymentDate, loan.InterestRate, loan.NoMonths, loan.MonthlyPaymentAmount,
                                               loan.State, loan.MonthlyPaymentsCompleted, loan.RepaidAmount, loan.Penalty);
-                    
 
-                    
+                    loanComponent.LoanUpdated += OnLoansUpdated;
+
                     LoansContainer.Items.Add(loanComponent);
                 }
             }
