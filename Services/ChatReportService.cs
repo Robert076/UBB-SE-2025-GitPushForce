46e4bda9c8b83df1d375b9bdbe436d0e0f017108
laithha <147397351+laithha@users.noreply.github.com>
2025-03-30 17:42:43 +0300

    done

--- Services/ChatReportService.cs
+++ Services/ChatReportService.cs
@@ -24,7 +24,7 @@ namespace src.Services
 
         public async Task<bool> SolveChatReport(ChatReport chatReportToBeSolved)
         {
-           
+
             string reportedMessage = chatReportToBeSolved.ReportedMessage;
 
             bool isReportedMessageOffensive = await IsMessageOffensive(reportedMessage);
@@ -61,10 +61,17 @@ namespace src.Services
             _chatReportRepository.DeleteChatReport(chatReportToBeSolved.Id);
             TipsService service = new TipsService();
             service.GiveTipToUser(chatReportToBeSolved.ReportedUserCNP);
-
-            MessagesService services = new MessagesService();
-            services.GiveMessageToUser(chatReportToBeSolved.ReportedUserCNP);
-
+            SqlParameter[] tipsParameters = new SqlParameter[]
+            {
+                 new SqlParameter("@UserCNP", chatReportToBeSolved.ReportedUserCNP)
+            };
+            
+            int countTips = dbConn.ExecuteScalar<int>("GetNumberOfGivenTipsForUser", tipsParameters, CommandType.StoredProcedure);
+            if (countTips % 3 == 0)
+            {
+                MessagesService services = new MessagesService();
+                services.GiveMessageToUser(chatReportToBeSolved.ReportedUserCNP);
+            }
 
             return true;
         }
