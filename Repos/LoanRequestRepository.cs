10a8effaf6b23deeebc4bed1bdd52eb39d0b7273
paulb <paulberca@gmail.com>
2025-03-30 21:11:05 +0300

    added new methods that communicate with the db

--- Repos/LoanRequestRepository.cs
+++ Repos/LoanRequestRepository.cs
@@ -40,7 +40,8 @@ namespace src.Repos
                         row["UserCNP"].ToString(),
                         Convert.ToSingle(row["Amount"]),
                         Convert.ToDateTime(row["ApplicationDate"]),
-                        Convert.ToDateTime(row["RepaymentDate"])
+                        Convert.ToDateTime(row["RepaymentDate"]),
+                        row["State"].ToString()
                     );
 
                     loanRequests.Add(loanRequest);
@@ -54,17 +55,72 @@ namespace src.Repos
             }
         }
 
+        public List<LoanRequest> GetUnsolvedLoanRequests()
+        {
+            try
+            {
+                DataTable? dataTable = dbConn.ExecuteReader("GetUnsolvedLoanRequests", null, CommandType.StoredProcedure);
+
+                if (dataTable == null || dataTable.Rows.Count == 0)
+                {
+                    throw new Exception("Loan requests table is empty");
+                }
+
+                List<LoanRequest> loanRequests = new List<LoanRequest>();
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    LoanRequest loanRequest = new LoanRequest(
+                        Convert.ToInt32(row["ID"]),
+                        row["UserCNP"].ToString(),
+                        Convert.ToSingle(row["Amount"]),
+                        Convert.ToDateTime(row["ApplicationDate"]),
+                        Convert.ToDateTime(row["RepaymentDate"]),
+                        row["State"].ToString()
+                    );
+
+                    loanRequests.Add(loanRequest);
+                }
+
+                return loanRequests;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Error - GetLoanRequests: {exception.Message}");
+            }
+        }
+
+        public void SolveLoanRequest(Int32 loanRequestID)
+        {
+            try
+            {
+                SqlParameter[] parameters = new SqlParameter[]
+                {
+                    new SqlParameter("@LoanRequestID", loanRequestID)
+                };
+
+                int rowsAffected = dbConn.ExecuteNonQuery("MarkRequestAsSolved", parameters, CommandType.StoredProcedure);
+
+                if (rowsAffected == 0)
+                {
+                    throw new Exception($"No loan request was found with Id {loanRequestID}");
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Error - MarkRequestAsSolved: {exception.Message}");
+            }
+        }
+
         public void DeleteLoanRequest(Int32 loanRequestID)
         {
             try
             {
                 SqlParameter[] parameters = new SqlParameter[]
                 {
-                    new SqlParameter("@ID", loanRequestID)
+                    new SqlParameter("@LoanRequestID", loanRequestID)
                 };
-
                 int rowsAffected = dbConn.ExecuteNonQuery("DeleteLoanRequest", parameters, CommandType.StoredProcedure);
-
                 if (rowsAffected == 0)
                 {
                     throw new Exception($"No loan request was found with Id {loanRequestID}");
